8c2207ef483ab9780530b56d0611cfd685a07c68
K_ <119788148+K0210@users.noreply.github.com>
2024-01-28 22:19:21 +0900

    Update main.js

--- sience/main.js
+++ sience/main.js
@@ -2,6 +2,7 @@ $(function(){
   $('.department2').on('click', function() {
     if ($(this).prop('checked')){
       $('.department1').prop('checked', false);
+      $('.department3').prop('checked', false);
       $(this).prop('checked', true);
     }
   });
@@ -9,6 +10,16 @@ $(function(){
 $(function(){
   $('.department1').on('click', function() {
     if ($(this).prop('checked')){
+      $('.department2').prop('checked', false);
+      $('.department3').prop('checked', false);
+      $(this).prop('checked', true);
+    }
+  });
+});
+$(function(){
+  $('.department3').on('click', function() {
+    if ($(this).prop('checked')){
+      $('.department1').prop('checked', false);
       $('.department2').prop('checked', false);
       $(this).prop('checked', true);
     }
