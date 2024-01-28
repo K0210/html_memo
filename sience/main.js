a6d7629f0731c71bc9245024b6dd648992b29b7f
K_ <119788148+K0210@users.noreply.github.com>
2024-01-28 21:01:52 +0900

    Update main.js

--- sience/main.js
+++ sience/main.js
@@ -1,7 +1,6 @@
 $(function(){
   $('.department2').on('click', function() {
     if ($(this).prop('checked')){
-      // 一旦全てをクリアして再チェックする
       $('.department1').prop('checked', false);
       $(this).prop('checked', true);
     }
@@ -10,7 +9,6 @@ $(function(){
 $(function(){
   $('.department1').on('click', function() {
     if ($(this).prop('checked')){
-      // 一旦全てをクリアして再チェックする
       $('.department2').prop('checked', false);
       $(this).prop('checked', true);
     }
