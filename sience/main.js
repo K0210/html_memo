e54c076ac5a7416bb7aaf8d008e60a70aa1ff7f2
K_ <119788148+K0210@users.noreply.github.com>
2024-01-28 19:16:56 +0900

    Update main.js

--- sience/main.js
+++ sience/main.js
@@ -1,18 +1,12 @@
 const department1 = document.getElementsByClassName("department1");
 const department2 = document.getElementsByClassName("department2");
-for(let i=0;i<department2.length;i++){
-　　department2[i].onclick = function(){
+function1(){
       for(let u=0;u<department1.length;u++){
-        department1[u].checked = false;
-        this.checked = true;
+            department1[u].checked = false;
       }
-  　}
-} 
-for(let i=0;i<department1.length;i++){
-　　department1[i].onclick = function(){
+}
+function2(){
       for(let u=0;u<department2.length;u++){
         department2[u].checked = false;
-        this.checked = true;
       }
-  　}
 } 
