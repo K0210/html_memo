59a60acbf574937023548a0ac58ffa14f3377d62
K_ <119788148+K0210@users.noreply.github.com>
2024-01-28 19:00:43 +0900

    Update main.js

--- sience/main.js
+++ sience/main.js
@@ -1,5 +1,5 @@
 const department1 = document.getElementsByClassName("department1");
-const department2 = document.getElementsByClassName("department2")
+const department2 = document.getElementsByClassName("department2");
 for(let i=0;i<department2.length;i++){
 　　department2[i].onclick = function(){
       for(let u=0;u<department1.length;u++){
