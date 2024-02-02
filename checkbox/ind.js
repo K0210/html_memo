585375265c1972810b4b2c26dcf76d3521744177
K_ <119788148+K0210@users.noreply.github.com>
2024-02-02 20:06:38 +0900

    Update ind.js

--- checkbox/ind.js
+++ checkbox/ind.js
@@ -1,6 +1,4 @@
-function(){
-  let jk = document.getElementById('jk');
-  if(jk.checked = true){
-    alert(String.fromCodePoint(22909,12365,12391,12377,12290,13,10,20184,12365,21512,12387,12390,12367,12384,12373,12356,12290));
-  };
-}
+let jk = document.getElementById('jk');
+if(jk.checked){
+  alert(String.fromCodePoint(22909,12365,12391,12377,12290,13,10,20184,12365,21512,12387,12390,12367,12384,12373,12356,12290));
+};
