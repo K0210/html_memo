d5eba24f13bbcdc7a6187f76502caf4f369e0bba
K_ <119788148+K0210@users.noreply.github.com>
2024-02-02 19:15:55 +0900

    Create ind.js

---
+++ checkbox/ind.js
@@ -0,0 +1,6 @@
+function(){
+  let element = document.getElementById('jk');
+  if(element.checked){
+    alert(String.fromCodePoint(22909,12365,12391,12377,12290,13,10,20184,12365,21512,12387,12390,12367,12384,12373,12356,12290));
+  };
+}
