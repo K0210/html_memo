93ae05a01064742a364cad1b4e48c281738be6cb
K_ <119788148+K0210@users.noreply.github.com>
2024-02-02 20:13:26 +0900

    Update ind.js

--- checkbox/ind.js
+++ checkbox/ind.js
@@ -1,4 +1,6 @@
 let jk = document.getElementById('jk');
-if(jk.checked){
-  alert(String.fromCodePoint(22909,12365,12391,12377,12290,13,10,20184,12365,21512,12387,12390,12367,12384,12373,12356,12290));
+switch(jk.checked){
+  case true;
+    alert(String.fromCodePoint(22909,12365,12391,12377,12290,13,10,20184,12365,21512,12387,12390,12367,12384,12373,12356,12290));
+    break;
 };
