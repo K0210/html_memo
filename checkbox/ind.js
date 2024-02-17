7e9783e7741e9ee94f7f91952e3c46e9611d86d4
K_ <119788148+K0210@users.noreply.github.com>
2024-02-17 21:09:33 +0900

    Update ind.js

--- checkbox/ind.js
+++ checkbox/ind.js
@@ -1,6 +1,4 @@
 let jk = document.getElementById('jk');
-switch(jk){
-  case checked;
-    alert(String.fromCodePoint(22909,12365,12391,12377,12290,13,10,20184,12365,21512,12387,12390,12367,12384,12373,12356,12290));
-    break;
+if(jk.checked){
+  alert(String.fromCodePoint(22909,12365,12391,12377,12290,13,10,20184,12365,21512,12387,12390,12367,12384,12373,12356,12290));
 };
