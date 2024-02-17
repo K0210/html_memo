ef12ab39eb27b0ecd85489ba04b5f8632f6b8f2f
K_ <119788148+K0210@users.noreply.github.com>
2024-02-17 21:13:25 +0900

    Update ind.js

--- checkbox/ind.js
+++ checkbox/ind.js
@@ -1,4 +1,4 @@
-let jk = document.getElementById('jk');
+var jk = document.getElementById('jk');
 if(jk.checked){
   alert(String.fromCodePoint(22909,12365,12391,12377,12290,13,10,20184,12365,21512,12387,12390,12367,12384,12373,12356,12290));
 };
