ebc96e1260ba012b1d800c0c44036b5dc2f7e93e
K_ <119788148+K0210@users.noreply.github.com>
2024-09-01 01:44:13 +0900

    Update multiple pages.js

--- multiple pages.js
+++ multiple pages.js
@@ -1,1 +1,15 @@
+let current;
+function goto(to_page) {
+  let page = document.getElementById(current);
+  page.style.visibility = "hidden";
+  current = to_page;
+  page = document.getElementById(current);
+  page.style.visibility = "visible";
+}
 
+function main() {
+  current = "p1";
+  goto("p1");
+}
+
+window.onload = main;
